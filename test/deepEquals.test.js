34da634496068a53f566b19c900c11660660d57c
dan.castillo <dan.castillo@jasper.ai>
2024-08-12 09:24:35 -0400

    fix: add c8 dev dependency and update node test files

--- test/deepEquals.test.js
+++ test/deepEquals.test.js
@@ -1,5 +1,4 @@
-import test, { describe } from 'node:test'
-import assert from 'node:assert/strict';
+import { describe, test } from 'node:test'
 import { deepEqual } from '../lib/deepEqual.js'
 
 describe('deepEquals', () => {
@@ -7,11 +6,28 @@ describe('deepEquals', () => {
   const b = { a: 1 }
   const c = { c: 'foo' }
 
-  test('passing test', () => {
-    assert.strictEqual(deepEqual(a, b), true)
+  test('passing test', (t) => {
+    t.plan(1)
+    t.assert.strictEqual(deepEqual(a, b), true)
   })
 
-  test('failing test', () => {
-    assert.strictEqual(deepEqual(a, c), false)
+  test('failing test', (t) => {
+    t.plan(1)
+    t.assert.strictEqual(deepEqual(a, c), false)
+  })
+
+  test('return false for not typeof a', (t) => {
+    t.plan(1)
+    t.assert.strictEqual(deepEqual(null, b), false)
+  })
+
+  test('return false for not typeof b', (t) => {
+    t.plan(1)
+    t.assert.strictEqual(deepEqual(a, null), false)
+  })
+
+  test('return false for unequal obj lens', (t) => {
+    t.plan(1)
+    t.assert.strictEqual(deepEqual(a, { a: true, b: true, c: true }), false)
   })
 })
